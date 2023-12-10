078018754cfd0c265bc3f54a2b880f58d03f385f
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-10 15:00:33 -0500

    Fix association declarations in Meeting and Resource models

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,8 +1,8 @@
 class Meeting < ApplicationRecord
    belongs_to :student
    belongs_to :mentor
-   has_many :resources :through => :resource_meetings, dependent: :destroy
+   has_many :resource_meetings, dependent: :destroy
+   has_many :resources, through: :resource_meetings
 
    validates :student_id, :mentor_id, :date, :time, presence: true
-
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,4 @@
 class Resource < ApplicationRecord
-   has_many :meetings :through => :resource_meetings
+   has_many :resource_meetings
+   has_many :meetings, through: :resource_meetings
 end
