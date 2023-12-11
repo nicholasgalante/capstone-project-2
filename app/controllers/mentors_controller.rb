4ed2c5ddd7e39e745084d751698de635c585db3a
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-11 16:49:55 -0500

    Add authorization and error handling to application and mentors controllers

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,2 +1,24 @@
 class MentorsController < ApplicationController
+   before_action :authorize, only: [:show]
+   
+   def show
+      mentor = Mentor.find_by(id: session[:mentor_id])
+      render json: mentor
+   end
+   
+   def create
+      mentor = Mentor.create(mentor_params)
+      if mentor.valid?
+         session[:mentor_id] = mentor.id
+         render json: mentor
+      else
+         render json: { errors: mentor.errors.full_messages }, status: :unprocessable_entity
+      end
+   end
+   
+   private
+   
+   def mentor_params
+      params.permit(:name, :email, :password, :password_confirmation)
+   end
 end
