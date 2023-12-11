4f9f95856db6f880edfdd404d47e0133f56579ba
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-11 16:42:03 -0500

    Add missing validations to Meeting and MentorApplication models

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -4,5 +4,5 @@ class Meeting < ApplicationRecord
    has_many :resource_meetings, dependent: :destroy
    has_many :resources, through: :resource_meetings
 
-   validates :student_id, :mentor_id, presence: true
+   validates :student_id, :mentor_id, :meeting_datetime, :location, :topics_discussed, :next_steps, presence: true
 end
--- app/models/mentor_application.rb
+++ app/models/mentor_application.rb
@@ -1,3 +1,5 @@
 class MentorApplication < ApplicationRecord
    belongs_to :mentor
+
+   validates :mentor_id, :resume_url, :motivation, :goals_aspirations, :philosophy, presence: true
 end
