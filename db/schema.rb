52954ae3389c229633dfc5741773eaec361c4d5a
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-11 13:03:14 -0500

    Update schema.rb with new columns for mentor and student IDs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_10_200527) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_11_180218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_10_200527) do
     t.text "next_steps"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "mentor_id"
+    t.string "student_id"
   end
 
   create_table "mentor_applications", force: :cascade do |t|
