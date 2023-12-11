fdfe4faac91add2d8fc36fb24489522e798307d6
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-11 17:16:08 -0500

    Remove unnecessary resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  resources :meeting_resources
-  resources :resources
-  resources :meetings
-  resources :student_applications
-  resources :mentor_applications
-  resources :students
-  resources :mentors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,7 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  #index all mentors
   get "/mentors" => "mentors#index"
   get "/mentors/:id" => "mentors#show"
   get "/students" => "students#index"
