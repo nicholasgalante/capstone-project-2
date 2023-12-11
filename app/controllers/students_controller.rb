992205a31d14a746a49ffd96fd581a244b8b4baa
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-11 16:56:38 -0500

    Add before_action and show action to StudentsController

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,24 @@
 class StudentsController < ApplicationController
+   before_action :authorize, only: [:show]
+   
+   def show
+      student = Student.find_by(id: session[:student_id])
+      render json: student
+   end
+   
+   def create
+      student = Student.create(student_params)
+      if student.valid?
+         session[:student_id] = student.id
+         render json: student
+      else
+         render json: { errors: student.errors.full_messages }, status: :unprocessable_entity
+      end
+   end
+   
+   private
+   
+   def student_params
+      params.permit(:name, :email, :password, :password_confirmation)
+   end
 end
