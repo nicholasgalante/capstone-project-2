c9029abce0199d44f5e4529ace86635d843dcf69
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-11 14:39:16 -0500

    Add validations to Mentor and Student models

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -3,5 +3,8 @@ class Mentor < ApplicationRecord
 
    has_one :student
    has_many :meetings, dependent: :destroy
-   
+
+   validates :first_name, :last_name, :email_address, :company_name, :job_title, presence: true
+   validates :password, confirmation: true
+   validates :email_address, uniqueness: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,7 +4,9 @@ class Student < ApplicationRecord
    belongs_to :mentor
    has_many :meetings, dependent: :destroy
 
-   validates :first_name, :last_name, :email_address, presence: true
+   validates :password, confirmation: true
+   validates :first_name, :last_name, :email_address, :degree_type, :area_of_study, :expected_graduation_date, :university_name, presence: true
+   validates :password, length: { minimum: 8, message: "must be at least 8 characters long" }
    validates :email_address, uniqueness: true
 
 end
