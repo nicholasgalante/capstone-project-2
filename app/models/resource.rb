9c48d1cfc77bfbba02ffa98fe014dc82015615e9
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-12 16:34:59 -0500

    Update validation for resource name to title

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,5 +2,5 @@ class Resource < ApplicationRecord
    has_many :resource_meetings
    has_many :meetings, through: :resource_meetings
 
-   validates :name, :url, presence: true    
+   validates :title, :url, presence: true    
 end
