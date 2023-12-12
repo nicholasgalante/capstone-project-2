da31767403c272ef2dc0a56ec5d54b145af7fb6c
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-12 16:32:16 -0500

    Refactor Mentor model validations and add downcase_email callback

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,11 +1,18 @@
 class Mentor < ApplicationRecord
-   has_secure_password
+  has_secure_password
+  before_save :downcase_email
 
-   has_one :student
-   has_many :meetings, dependent: :destroy
+  has_one :student
+  has_many :meetings, dependent: :destroy
 
-   validates :first_name, :last_name, :email_address, :company_name, :job_title, presence: true
-   validates :password, confirmation: true
-   #validates :password, length: { minimum: 6, message: "must be at least 6 characters long" }
-   validates :email_address, uniqueness: true
+  validates :first_name, :last_name, :email_address, :company_name, :job_title, presence: true
+  validates :password, confirmation: true
+  #validates :password, length: { minimum: 6, message: "must be at least 6 characters long" }
+  validates :email_address, uniqueness: true
+
+  private
+
+  def downcase_email
+    self.email_address = email_address.downcase
+  end
 end
