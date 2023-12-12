3985ba5e6062eff9111032bc0287fc15b2a491f6
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-12 18:19:11 -0500

    Update StudentSerializer attributes

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,3 +1,3 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email_address, :password_digest, :university_name, :degree_type, :area_of_study
+  attributes :id, :first_name, :last_name, :email_address, :university_name, :degree_type, :area_of_study, :expected_graduation_date, :mentor_id
 end
