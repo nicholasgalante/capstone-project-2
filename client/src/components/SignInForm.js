e9f132e57039d7da15030a39613a5e90ed534de5
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-12 00:01:10 -0500

    Add SignInForm component

---
+++ client/src/components/SignInForm.js
@@ -0,0 +1,54 @@
+import React, { useState, useContext } from "react";
+
+function SignInForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState([]);
+
+  function handleSubmit(e) {
+      e.preventDefault();
+      setIsLoading(true);
+      setErrors([]);
+   
+      fetch("/signin", {
+         method: "POST",
+         headers: {
+         "Content-Type": "application/json",
+         },
+         body: JSON.stringify({ email, password }),
+      }).then((r) => {
+         setIsLoading(false);
+         if (r.ok) {
+         r.json().then((user) => {
+            console.log(user);
+         });
+         } else {
+         r.json().then((err) => setErrors(err.errors));
+         }
+      });
+   }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        placeholder="Password"
+      />
+      <button type="submit">{isLoading ? "Loading..." : "Sign in"}</button>
+      {errors.map((err) => (
+        {err}
+      ))}
+    </form>
+  );
+}
+
+export { SignInForm };
