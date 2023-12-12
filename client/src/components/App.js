ba9d9984efe8627ee542b878661c88e6c8079cc8
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-12 18:19:15 -0500

    Add StudentSignUpForm component to App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import { UserProvider } from "../context/UserContext.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { SignInForm } from "./SignInForm";
+import { StudentSignUpForm } from "./StudentSignUpForm";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navbar />} />
-          <Route path="/signin" element={<SignInForm />} />          
+          <Route path="/signin" element={<SignInForm />} /> 
+          <Route path="/signup/student" element={<StudentSignUpForm />} />         
         </Routes>
       </BrowserRouter>
     </UserProvider>
