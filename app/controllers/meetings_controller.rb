8cb105bdd2abb97db726f7a4725636392920966f
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-14 13:30:08 -0500

    Add CRUD actions to Meetings controller

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,32 @@
 class MeetingsController < ApplicationController
+   skip_before_action :authorize
+
+   def index
+      meetings = Meeting.all
+      render json: meetings, status: :ok
+   end
+
+   def show
+      meeting = Meeting.find(params[:id])
+      render json: meeting, status: :ok
+   end
+
+   def create
+      meeting = Meeting.create(meeting_params)
+      render json: meeting, status: :created
+   end
+
+   def destroy
+      meeting = Meeting.find(params[:id])
+      meeting.destroy
+      head :no_content
+   end
+
+   private
+
+   def meeting_params
+      params.require(:meeting). permit(:mentor_id, :student_id, :meeting_date, :meeting_time, :meeting_location, :meeting_notes)
+   end
+
 end
+
