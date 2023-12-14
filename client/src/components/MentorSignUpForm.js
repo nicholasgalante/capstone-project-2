af9f9cf23750ebf740591dcae5c7af0d01993efd
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-14 12:18:00 -0500

    Refactor MentorSignUpForm component to use formData object

--- client/src/components/MentorSignUpForm.js
+++ client/src/components/MentorSignUpForm.js
@@ -3,19 +3,25 @@ import { UserContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 
 function MentorSignUpForm() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirmation, setPasswordConfirmation] = useState("");
-  const [companyName, setCompanyName] = useState("");
-  const [jobTitle, setJobTitle] = useState("");
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    companyName: "",
+    jobTitle: "",
+  });
   const [isLoading, setIsLoading] = useState(false);
   const [errors, setErrors] = useState([]);
   const { setUser } = useContext(UserContext);
   const navigate = useNavigate();
 
-  function handleSubmit(e) {
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
     setErrors([]);
@@ -25,15 +31,8 @@ function MentorSignUpForm() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        mentor: {
-          first_name: firstName,
-          last_name: lastName,
-          email_address: email,
-          password: password,
-          password_confirmation: passwordConfirmation,
-          company_name: companyName,
-          job_title: jobTitle,
-        },
+        mentor: formData,
+        credentials: 'include', // <-- includes cookies in the request
       }),
     }).then((r) => {
       setIsLoading(false);
@@ -46,51 +45,58 @@ function MentorSignUpForm() {
         r.json().then((err) => setErrors(err.errors));
       }
     });
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>Sign Up</label>
       <input
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
+        value={formData.firstName}
+        onChange={handleChange}
         type="text"
+        name="firstName"
         placeholder="First Name"
       />
       <input
-        value={lastName}
-        onChange={(e) => setLastName(e.target.value)}
+        value={formData.lastName}
+        onChange={handleChange}
         type="text"
+        name="lastName"
         placeholder="Last Name"
       />
       <input
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        value={formData.email}
+        onChange={handleChange}
         type="text"
+        name="email"
         placeholder="Email"
       />
       <input
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        value={formData.password}
+        onChange={handleChange}
         type="password"
+        name="password"
         placeholder="Password"
       />
       <input
-        value={passwordConfirmation}
-        onChange={(e) => setPasswordConfirmation(e.target.value)}
+        value={formData.passwordConfirmation}
+        onChange={handleChange}
         type="password"
+        name="passwordConfirmation"
         placeholder="Password Confirmation"
       />
       <input
-        value={companyName}
-        onChange={(e) => setCompanyName(e.target.value)}
+        value={formData.companyName}
+        onChange={handleChange}
         type="text"
+        name="companyName"
         placeholder="Company Name"
       />
       <input
-        value={jobTitle}
-        onChange={(e) => setJobTitle(e.target.value)}
+        value={formData.jobTitle}
+        onChange={handleChange}
         type="text"
+        name="jobTitle"
         placeholder="Job Title"
       />
       <button type="submit">{isLoading ? "Loading..." : "Sign Up"}</button>
@@ -99,4 +105,4 @@ function MentorSignUpForm() {
   );
 }
 
-export  {MentorSignUpForm};
+export { MentorSignUpForm };
