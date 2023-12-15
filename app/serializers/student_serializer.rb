1439591c2e0d36db567122761e0d9dd490fcbd58
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-15 13:20:04 -0500

    Add associations to StudentSerializer

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,3 +1,6 @@
 class StudentSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :email_address, :university_name, :degree_type, :area_of_study, :expected_graduation_date, :mentor_id
+  has_many :meetings
+  has_one :student_application
+  has_one :mentor
 end
