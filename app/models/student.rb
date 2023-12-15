15c62c0c3d789be06e2466d72b445747af881be7
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-15 13:19:51 -0500

    Add student_application association to Student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,7 @@ class Student < ApplicationRecord
 
   belongs_to :mentor, optional: true
   has_many :meetings, dependent: :destroy
+  has_one :student_application
 
   #validates :first_name, :last_name, :email_address, :degree_type, :area_of_study, :university_name, presence: true
   validates :password, confirmation: true
