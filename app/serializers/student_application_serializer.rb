2ca12397589949bc382a81611100c535579a24e2
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-15 13:20:20 -0500

    Remove expected_graduation attribute from StudentApplicationSerializer

--- app/serializers/student_application_serializer.rb
+++ app/serializers/student_application_serializer.rb
@@ -1,3 +1,3 @@
 class StudentApplicationSerializer < ActiveModel::Serializer
-  attributes :id, :application_status, :expected_graduation, :current_gpa, :major, :motivation, :portfolio_url, :goals_aspirations
+  attributes :id, :application_status, :current_gpa, :major, :motivation, :portfolio_url, :goals_aspirations
 end
