10735ba560f8b05f417b449aa309d08210cee5e2
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-15 13:19:57 -0500

    Add attributes and associations to MeetingSerializer and MentorSerializer

--- app/serializers/meeting_serializer.rb
+++ app/serializers/meeting_serializer.rb
@@ -1,3 +1,3 @@
 class MeetingSerializer < ActiveModel::Serializer
-  attributes :id, :organizer_id, :meeting_datetime, :location, :topics_discussed, :next_steps
+  attributes :id, :student_id, :mentor_id, :organizer_id, :meeting_datetime, :location, :topics_discussed, :next_steps
 end
--- app/serializers/mentor_serializer.rb
+++ app/serializers/mentor_serializer.rb
@@ -1,3 +1,6 @@
 class MentorSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :email_address, :password_digest, :company_name, :job_title
+  has_many :meetings
+  has_one :mentor_application
+  has_one :student
 end
