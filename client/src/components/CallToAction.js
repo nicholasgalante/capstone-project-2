8f6f30bc440e052414c62f94882b32a06628aa30
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-23 13:26:14 -0500

    Add react-router-dom and update CallToAction component

--- client/src/components/CallToAction.js
+++ client/src/components/CallToAction.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function CallToAction() {
    return (
      <div className="bg-white">
@@ -13,15 +15,14 @@ export default function CallToAction() {
            Start your personalized one-to-one mentorship journey today and unlock your career potential.
            </p>
            <div className="mt-10 flex items-center justify-center gap-x-6">
-             <a
-               href="#"
+           <Link to="/signup"><a
                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
              >
                Get started
-             </a>
-             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+             </a></Link>
+             <Link to="/signin"><a className="text-sm font-semibold leading-6 text-gray-900">
                Sign In <span aria-hidden="true">→</span>
-             </a>
+             </a></Link>
            </div>
          </div>
        </div>
