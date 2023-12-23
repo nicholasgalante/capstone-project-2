bbd53a63a647916c7e2685ca42ffe08b3f5230e0
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-23 13:26:02 -0500

    Add SignInPage component

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,15 @@ import { SignUpPage } from "./pages/SignUpPage.js";
 import { MeetingsPage } from "./pages/MeetingsPage.js";
 import { MeetingDetailPage } from "./pages/MeetingDetailPage.js";
 import { HomePage } from "./pages/HomePage.js";
+import { SignInPage } from "./pages/SignInPage.js";
 
 function App() {
   return (
     <UserProvider>
       <BrowserRouter>
-      {/* <Navbar />   */}
         <Routes>
           <Route path="/" element={<HomePage/>} />
-          <Route path="/signin" element={<SignInForm />} /> 
+          <Route path="/signin" element={<SignInPage />} />     
           <Route path="/signup" element={<SignUpPage />} /> 
           <Route path="/meetings" element={<MeetingsPage />} />
           <Route path="/meetings/:meetingID" element={<MeetingDetailPage />} />
