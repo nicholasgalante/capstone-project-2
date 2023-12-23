5a01e4bf0b30f46d290a2e342a8a34c2228ef838
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-23 13:26:27 -0500

    Refactor SignUpPage component

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,38 +1,127 @@
 import React, { useState } from "react";
 import { StudentSignUpForm } from "../components/StudentSignUpForm";
 import { MentorSignUpForm } from "../components/MentorSignUpForm";
+import { AccountTypeSelector } from "../components/AccountTypeSelector";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 function SignUpPage() {
   const [userType, setUserType] = useState("student");
 
-  const handleUserTypeChange = (event) => {
-    setUserType(event.target.value);
-  };
-
   return (
-    <div>
-      Sign Up Page
-      <div>
-        <label>
-          <input
-            type="radio"
-            value="student"
-            checked={userType === "student"}
-            onChange={handleUserTypeChange}
-          />
-          Student
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="mentor"
-            checked={userType === "mentor"}
-            onChange={handleUserTypeChange}
-          />
-          Mentor
-        </label>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm ">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Create an account
+        </h2>
       </div>
+
+      <nav className="flex justify-center space-x-4 mt-6" aria-label="Tabs">
+        <button
+          value="student"
+          onClick={() => setUserType("student")}
+          className={classNames(
+            userType === "student"
+              ? "bg-indigo-100 text-indigo-700"
+              : "text-gray-500 hover:text-gray-700",
+            "rounded-md px-3 py-2 text-sm font-medium"
+          )}
+        >
+          Student
+        </button>
+        <button
+          value="mentor"
+          onClick={() => setUserType("mentor")}
+          className={classNames(
+            userType === "mentor"
+              ? "bg-indigo-100 text-indigo-700"
+              : "text-gray-500 hover:text-gray-700",
+            "rounded-md px-3 py-2 text-sm font-medium"
+          )}
+        >
+          Mentor
+        </button>
+      </nav>
+
       {userType === "student" ? <StudentSignUpForm /> : <MentorSignUpForm />}
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Create Account
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Already have an account?{" "}
+          <a
+            href="#"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Sign In
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
