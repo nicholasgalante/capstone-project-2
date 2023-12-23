2875e1ba5b08dd4721e094288a0bd672f4f3d6eb
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-23 10:10:23 -0500

    Add HomePage component

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Hero } from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import TestimonialSection from "../components/TestimonialSection";
+import CallToAction from "../components/CallToAction";
+import FAQSection from "../components/FAQSection";
+import Footer from "../components/Footer";
+import StatsSection from "../components/StatsSection";
+
+function HomePage() {
+  return (
+    <div>
+      <Hero />
+      <AboutSection />
+      <StatsSection />
+      <TestimonialSection />
+      <FAQSection />
+      <CallToAction />
+      <Footer/>
+    </div>
+  );
+}
+
+export { HomePage };
