4307e3880dbff4ec6abdd39b52a0e4b3ecdcfb08
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-23 13:26:17 -0500

    Update HeroSection.js with Sign In and Sign Up links

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -38,18 +38,16 @@ function Hero() {
               mentorships between university students and career professionals.{" "}
             </p>
             <div className="mt-10 flex items-center gap-x-6">
-              <a
-                href="#"
+            <Link to="/signup"><a
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Sign In <span aria-hidden="true">→</span>
-              </a>
+              </a></Link>
+              <Link to="/signin">
+                <a className="text-sm font-semibold leading-6 text-gray-900">
+                  Sign In <span aria-hidden="true">→</span>
+                </a>
+              </Link>
             </div>
           </div>
         </div>
