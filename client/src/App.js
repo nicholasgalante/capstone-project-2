6dd8207be4f1940dbb1ddb7f8e11c202753b6d6b
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-27 10:34:26 -0500

    Update routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
 import { UserProvider } from "./context/UserContext.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
-import { SignInForm } from "./components/SignInForm";
 import { SignUpPage } from "./pages/SignUpPage.js";
 import { MeetingsPage } from "./pages/MeetingsPage.js";
 import { MeetingDetailPage } from "./pages/MeetingDetailPage.js";
 import { HomePage } from "./pages/HomePage.js";
 import { SignInPage } from "./pages/SignInPage.js";
+import { Dashboard } from "./pages/Dashboard.js";
 
 function App() {
   return (
     <UserProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/signin" element={<SignInPage />} />     
-          <Route path="/signup" element={<SignUpPage />} /> 
-          <Route path="/meetings" element={<MeetingsPage />} />
-          <Route path="/meetings/:meetingID" element={<MeetingDetailPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route
+            path="/meetings"
+            element={
+              <>
+                <Navbar />
+                <MeetingsPage />
+              </>
+            }
+          />
+          <Route
+            path="/meetings/:meetingID"
+            element={
+              <>
+                <Navbar />
+                <MeetingDetailPage />
+              </>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </UserProvider>
