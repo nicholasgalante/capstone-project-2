256961867cbbdcf963c456186168745ae914d862
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-10 14:11:38 -0500

    Refactor association name in Meeting model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,8 +1,8 @@
 class Meeting < ApplicationRecord
    belongs_to :student
    belongs_to :mentor
-   has_many :resource_meetings, dependent: :destroy
-   has_many :resources, through: :resource_meetings
+   has_many :meeting_resources, dependent: :destroy
+   has_many :resources, through: :meeting_resources
 
    validates :student_id, :mentor_id, :organizer_id, :meeting_datetime, :location, presence: true
 end
