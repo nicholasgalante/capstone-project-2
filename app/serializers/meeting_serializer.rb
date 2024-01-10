ad474ffab6013dc3a1e8c6d749d3caf7826d7d25
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-10 14:11:44 -0500

    Add resources attribute to MeetingSerializer

--- app/serializers/meeting_serializer.rb
+++ app/serializers/meeting_serializer.rb
@@ -1,3 +1,3 @@
 class MeetingSerializer < ActiveModel::Serializer
-  attributes :id, :student_id, :mentor_id, :organizer_id, :meeting_datetime, :location, :topics_discussed, :next_steps
+  attributes :id, :student_id, :mentor_id, :organizer_id, :meeting_datetime, :location, :topics_discussed, :next_steps, :resources
 end
