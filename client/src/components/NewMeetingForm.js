c7ffc1db0e03ecf236ac30a7785cfb9132439bf2
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-11 14:42:03 -0500

    Add location label and update button text

--- client/src/components/NewMeetingForm.js
+++ client/src/components/NewMeetingForm.js
@@ -96,12 +96,15 @@ function NewMeetingForm() {
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
+        <div className="flex flex-col h-full">
+        <label className=" text-white ">Location</label>
         <button
-          type="submit"
-          className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          type="button"
+          className="mt-auto block rounded-md bg-indigo-600 px-3 py-1.5 max-h-10 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Create Meeting
+          Schedule
         </button>
+        </div>
         {errors.map((err) => err)}
       </form>
     </div>
