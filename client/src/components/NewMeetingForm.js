1cedc6a2b859e6b441efc898837380342790d160
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-11 15:20:59 -0500

    Fix button type in NewMeetingForm

--- client/src/components/NewMeetingForm.js
+++ client/src/components/NewMeetingForm.js
@@ -99,7 +99,7 @@ function NewMeetingForm() {
         <div className="flex flex-col h-full">
         <label className=" text-white ">Location</label>
         <button
-          type="button"
+          type="submit"
           className="mt-auto block rounded-md bg-indigo-600 px-3 py-1.5 max-h-10 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Schedule
