d56a80d3ab1d3f5f863efb04d58729868cf1e944
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-11 15:20:57 -0500

    Remove settings link from Navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -107,19 +107,6 @@ function Navbar() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
                             onClick={handleSignOut}
                             className={classNames(
                               active ? "bg-gray-100" : "",
