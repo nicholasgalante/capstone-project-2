0484e1d196ddf833c8044077b68beda909d05214
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-14 11:36:47 -0500

    Refactor MeetingsController actions and update meeting_params

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,37 +1,35 @@
 class MeetingsController < ApplicationController
+  def index
+    meetings = @current_user.meetings.all
+    render json: meetings, status: :ok
+  end
 
-   def index
-      meetings = @current_user.meetings.all
-      render json: meetings, status: :ok
-   end
+  def show
+    meeting = Meeting.find(params[:id])
+    render json: meeting, status: :ok
+  end
 
-   def show
-      meeting = Meeting.find(params[:id])
-      render json: meeting, status: :ok
-   end
+  def update
+    debugger
+    meeting = Meeting.find(params[:id])
+    meeting.update!(meeting_params)
+    render json: meeting, status: :ok
+  end
 
-   def update
-      meeting = Meeting.find(params[:id])
-      meeting.update!(meeting_params)
-      render json: meeting, status: :ok
-   end
+  def create
+    meeting = Meeting.create!(meeting_params)
+    render json: meeting, status: :created
+  end
 
-   def create
-      meeting = Meeting.create!(meeting_params)
-      render json: meeting, status: :created
-   end
+  def destroy
+    meeting = Meeting.find(params[:id])
+    meeting.destroy
+    head :no_content
+  end
 
-   def destroy
-      meeting = Meeting.find(params[:id])
-      meeting.destroy
-      head :no_content
-   end
+  private
 
-   private
-
-   def meeting_params
-      params.permit(:mentor_id, :student_id, :organizer_id, :location, :meeting_datetime, :topics_discussed, :next_steps)
-   end
-
+  def meeting_params
+    params.require(:meeting).permit(:id, :mentor_id, :student_id, :organizer_id, :location, :meeting_datetime, :topics_discussed, :next_steps, :resources)
+  end
 end
-
