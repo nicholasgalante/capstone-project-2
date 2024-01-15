29d3d5277c78b41a371403ede50c0a710cc4882d
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-14 21:40:56 -0500

    Add create and destroy actions to MeetingResourcesController

--- app/controllers/meeting_resources_controller.rb
+++ app/controllers/meeting_resources_controller.rb
@@ -1,2 +1,19 @@
 class MeetingResourcesController < ApplicationController
+
+   def create
+      meeting_resource = MeetingResource.create!(meeting_resource_params)
+      render json: meeting_resource, status: :created
+   end
+   
+   def destroy
+      meeting_resource = MeetingResource.find(params[:id])
+      meeting_resource.destroy
+      head :no_content
+   end
+   
+   private
+   
+   def meeting_resource_params
+      params.require(:meeting_resource).permit(:id, :meeting_id, :resource_id)
+   end
 end
