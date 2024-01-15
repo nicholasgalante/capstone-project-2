2621ae7cbcf417c6e447babc6b52e6d83adb7f8e
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-15 14:00:36 -0500

    Refactor ResourcesList component layout

--- client/src/components/ResourcesList.js
+++ client/src/components/ResourcesList.js
@@ -37,10 +37,7 @@ function ResourcesList({meetingId, onResourceAdded}) {
   }
 
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
-      <div className="mt-8 flow-root">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8 mt-8 flow-root -mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
               <table className="min-w-full divide-y divide-gray-300">
                 <thead className="bg-gray-50">
@@ -69,7 +66,7 @@ function ResourcesList({meetingId, onResourceAdded}) {
                           </td>
                         </Link>
                         <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <a className="text-gray-400 hover:text-indigo-600" onClick={()=>handleAdd(resource.id)}>
+                        <a className="text-indigo-600 hover:text-indigo-900" onClick={()=>handleAdd(resource.id)}>
                           Attach to Meeting
                         </a>
                         </td>
@@ -79,9 +76,6 @@ function ResourcesList({meetingId, onResourceAdded}) {
               </table>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
   );
 }
 
