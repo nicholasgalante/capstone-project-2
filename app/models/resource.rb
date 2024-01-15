59e2dbb7221b3ab24a69b171b6f64c3666dbef5f
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-15 14:56:57 -0500

    Add mentor association to Resource model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,7 @@
 class Resource < ApplicationRecord
    has_many :meeting_resources
    has_many :meetings, through: :meeting_resources
+   belongs_to :mentor
 
    validates :title, :url, presence: true    
 end
