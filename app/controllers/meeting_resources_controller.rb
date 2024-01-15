c4db4c3606dc161c9cd8c77b0dfe9aaededdbae9
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-14 22:35:05 -0500

    Remove debugger statement in create method

--- app/controllers/meeting_resources_controller.rb
+++ app/controllers/meeting_resources_controller.rb
@@ -1,7 +1,6 @@
 class MeetingResourcesController < ApplicationController
 
    def create
-      debugger
       meeting_resource = MeetingResource.create!(meeting_resource_params)
       render json: meeting_resource, status: :created
    end
