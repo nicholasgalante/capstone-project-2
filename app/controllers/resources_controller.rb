e54fae3a5b5787fbaabcb6ffdb1e7f46f80ef7e1
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-22 18:27:18 -0500

    Refactor ResourcesController methods

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,34 +1,38 @@
 class ResourcesController < ApplicationController
-   def index
-      if session[:user_type] == "mentor"
-         resources = Mentor.find(session[:user_id]).resources
-         byebug
-      elsif session[:user_type] == "student"
-         resources = Student.find(session[:user_id]).resources
-      end
-      # resources = Resource.all
-      render json: resources, status: :ok
-   end
+  def index
+    resources = Resource.all
+    render json: resources, status: :ok
+  end
 
-   def create
-      resource = Resource.create(resource_params)
-      render json: resource, status: :created
-   end
+  def my_resources
+    if session[:user_type] == "mentor"
+      resources = Mentor.find(session[:user_id]).resources
+    elsif session[:user_type] == "student"
+      resources = Student.find(session[:user_id]).resources
+    end
+    # resources = Resource.all
+    render json: resources, status: :ok
+  end
 
-   def show
-      resource = Resource.find(params[:id])
-      render json: resource, status: :ok
-   end
+  def create
+    resource = Resource.create(resource_params)
+    render json: resource, status: :created
+  end
 
-   def destroy
-      resource = Resource.find(params[:id])
-      resource.destroy
-      head :no_content
-   end
+  def show
+    resource = Resource.find(params[:id])
+    render json: resource, status: :ok
+  end
 
-   private
+  def destroy
+    resource = Resource.find(params[:id])
+    resource.destroy
+    head :no_content
+  end
 
-   def resource_params
-      params.require(:resource).permit(:title, :url, :description)
-   end
+  private
+
+  def resource_params
+    params.require(:resource).permit(:title, :url, :description)
+  end
 end
