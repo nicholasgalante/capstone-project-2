3a2b4dad74e6719597bd8d5760f2d6fe1a85d6be
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-23 13:56:22 -0500

    Remove commented out code in MeetingDetailPage.js

--- client/src/pages/MeetingDetailPage.js
+++ client/src/pages/MeetingDetailPage.js
@@ -205,7 +205,6 @@ function MeetingDetailPage() {
                     name="comment"
                     id="comment"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    // defaultValue={location}
                     value={updatedData.location}
                     onChange={(e) =>
                       handleFieldChange("location", e.target.value)
@@ -225,7 +224,6 @@ function MeetingDetailPage() {
                     name="comment"
                     id="comment"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    // defaultValue={topics_discussed}
                     value={updatedData.topics_discussed}
                     onChange={(e) =>
                       handleFieldChange("topics_discussed", e.target.value)
@@ -245,7 +243,6 @@ function MeetingDetailPage() {
                     name="comment"
                     id="comment"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  focus:ring-inset focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    // defaultValue={next_steps}
                     value={updatedData.next_steps}
                     onChange={(e) =>
                       handleFieldChange("next_steps", e.target.value)
