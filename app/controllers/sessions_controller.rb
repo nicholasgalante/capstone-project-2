4039e54a4de14ace4c240cd3d494f854af6cbea0
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-22 20:42:16 -0500

    Refactor sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,31 +1,37 @@
 class SessionsController < ApplicationController
-   skip_before_action :authorize, only: [:create, :destroy]
-   
-   def create
-      email_address = params[:email_address].downcase
-      user = Mentor.find_by(email_address: email_address) || Student.find_by(email_address: email_address)
-      if user&.authenticate(params[:password])
-         session[:user_id] = user.id
-         session[:user_type] = user.class.name.downcase # "mentor" or "student"
-         session[:count] = (session[:count] || 0) + 1
-         render json: user, status: :created
-      else
-         render json: { errors: ["Invalid email or password"] }, status: :unauthorized
-      end
-      #Rails.logger.debug("Session data - SessionCreate: #{session.inspect}")
-   end
-   
-   def show
-      if @current_user
-        render json: @current_user, status: :ok
-      else
-        render json: { errors: ["Not authorized"] }, status: :unauthorized
-      end
-    end
+  skip_before_action :authorize, only: [:create, :destroy]
 
-   def destroy
-      session.delete :user_id
-      session.delete :user_type
-      head :no_content
-   end
+  def create
+    email_address = params[:email_address].downcase
+    user = Mentor.find_by(email_address: email_address) || Student.find_by(email_address: email_address)
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      session[:user_type] = user.class.name.downcase # "mentor" or "student"
+      render json: user, status: :created
+    else
+      render json: { errors: ["Invalid email or password"] }, status: :unauthorized
+    end
+  end
+
+  def show
+    if @current_user
+      render json: @current_user, status: :ok
+    else
+      render json: { errors: ["Not authorized"] }, status: :unauthorized
+    end
+  end
+
+  def user_type
+    if session[:user_type]
+      render json: { user_type: session[:user_type] }, status: :ok
+    else
+      render json: { errors: ["Not authorized"] }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    session.delete :user_type
+    head :no_content
+  end
 end
