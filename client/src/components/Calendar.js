ae167ffe8ca706f21b7fa9a233155bcb66efd171
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-24 13:30:11 -0500

    Fix bug in login functionality

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -25,6 +25,7 @@ import {
   getDay,
 } from "date-fns";
 import { UserContext } from "../context/UserContext";
+import { Link } from "react-router-dom";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -59,7 +60,7 @@ function Calendar() {
   function isDatetimeInMeetings(targetDatetime) {
     let formattedTargetDatetime = format(targetDatetime, "MM-dd-yyyy");
 
-    //if formattedTargetDatetime exists within user.meetings, return that meeting
+    //if formattedTargetDatetime exists within user.meetings, return that meeting id
     return user.meetings.find(meeting => {
       // Assuming each meeting object has a property for its date
       const meetingDate = format(meeting.meeting_datetime, "MM-dd-yyyy"); 
@@ -67,11 +68,14 @@ function Calendar() {
     });
   }
 
-  // return user.meetings.some(
-  //   (meeting) =>
-  //     format(meeting.meeting_datetime, "MM-dd-yyyy") ===
-  //     formattedTargetDatetime
-  // );
+  function getMeetingID(targetDatetime) {
+    let formattedTargetDatetime = format(targetDatetime, "MM-dd-yyyy");
+    return user.meetings.find(meeting => {
+      // Assuming each meeting object has a property for its date
+      const meetingDate = format(meeting.meeting_datetime, "MM-dd-yyyy"); 
+      return meetingDate === formattedTargetDatetime;
+    })
+  }
 
   return (
     <div className={"flex justify-center"}>
@@ -80,6 +84,7 @@ function Calendar() {
           <h1 className="text-base font-semibold leading-6 text-gray-900">
             <time dateTime="2022-01">{format(currentMonth, "MMMM yyyy")}</time>
           </h1>
+
           {/* Filter Dates Expanded View */}
           <div className="flex items-center">
             <div className="relative flex items-center rounded-md bg-white shadow-sm md:items-stretch">
@@ -109,16 +114,6 @@ function Calendar() {
               >
                 <span className="sr-only">Next month</span>
                 <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-              </button>
-            </div>
-
-            <div className="hidden md:ml-4 md:flex md:items-center">
-              <div className="ml-6 h-6 w-px bg-gray-300" />
-              <button
-                type="button"
-                className="ml-6 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Add event
               </button>
             </div>
 
@@ -154,86 +149,7 @@ function Calendar() {
                             "block px-4 py-2 text-sm"
                           )}
                         >
-                          Create event
-                        </a>
-                      )}
-                    </Menu.Item>
-                  </div>
-                  <div className="py-1">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active
-                              ? "bg-gray-100 text-gray-900"
-                              : "text-gray-700",
-                            "block px-4 py-2 text-sm"
-                          )}
-                        >
                           Go to today
-                        </a>
-                      )}
-                    </Menu.Item>
-                  </div>
-                  <div className="py-1">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active
-                              ? "bg-gray-100 text-gray-900"
-                              : "text-gray-700",
-                            "block px-4 py-2 text-sm"
-                          )}
-                        >
-                          Day view
-                        </a>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active
-                              ? "bg-gray-100 text-gray-900"
-                              : "text-gray-700",
-                            "block px-4 py-2 text-sm"
-                          )}
-                        >
-                          Week view
-                        </a>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active
-                              ? "bg-gray-100 text-gray-900"
-                              : "text-gray-700",
-                            "block px-4 py-2 text-sm"
-                          )}
-                        >
-                          Month view
-                        </a>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active
-                              ? "bg-gray-100 text-gray-900"
-                              : "text-gray-700",
-                            "block px-4 py-2 text-sm"
-                          )}
-                        >
-                          Year view
                         </a>
                       )}
                     </Menu.Item>
@@ -310,32 +226,11 @@ function Calendar() {
                   {/* Render Events */}
                   {isDatetimeInMeetings(day) ? (
                     <p className="flex-auto truncate font-medium text-gray-900 group-hover:text-indigo-600">
-                      {"Meeting"}
+                      <Link to={`/meetings`}>{"Meeting"}</Link>
                     </p>
                   ) : null}
-                  {/* {day.events.length > 0 && (
-                  <ol className="mt-2">
-                    {day.events.slice(0, 2).map((event) => (
-                      <li key={event.id}>
-                        <a href={event.href} className="group flex">
-                          <p className="flex-auto truncate font-medium text-gray-900 group-hover:text-indigo-600">
-                            {event.name}
-                          </p>
-                          <time
-                            dateTime={event.datetime}
-                            className="ml-3 hidden flex-none text-gray-500 group-hover:text-indigo-600 xl:block"
-                          >
-                            {event.time}
-                          </time>
-                        </a>
-                      </li>
-                    ))}
-                    {day.events.length > 2 && <li className="text-gray-500">+ {day.events.length - 2} more</li>}
-                  </ol>
-                )} */}
-                  {/* </div> */}
+              
                 </button>
-                // </div>
               ))}
             </div>
 
@@ -382,47 +277,11 @@ function Calendar() {
                   >
                     {format(day, "d")}
                   </time>
-
-                  {/* render events */}
-
-                  {/* <span className="sr-only">{day.events.length} events</span>
-                {day.events.length > 0 && (
-                  <span className="-mx-0.5 mt-auto flex flex-wrap-reverse">
-                    {day.events.map((event) => (
-                      <span key={event.id} className="mx-0.5 mb-1 h-1.5 w-1.5 rounded-full bg-gray-400" />
-                    ))}
-                  </span>
-                )} */}
                 </button>
               ))}
             </div>
           </div>
         </div>
-
-        {/* Events */}
-        {/* {selectedDay?.events.length > 0 && (
-        <div className="px-4 py-10 sm:px-6 lg:hidden">
-          <ol className="divide-y divide-gray-100 overflow-hidden rounded-lg bg-white text-sm shadow ring-1 ring-black ring-opacity-5">
-            {selectedDay.events.map((event) => (
-              <li key={event.id} className="group flex p-4 pr-6 focus-within:bg-gray-50 hover:bg-gray-50">
-                <div className="flex-auto">
-                  <p className="font-semibold text-gray-900">{event.name}</p>
-                  <time dateTime={event.datetime} className="mt-2 flex items-center text-gray-700">
-                    <ClockIcon className="mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    {event.time}
-                  </time>
-                </div>
-                <a
-                  href={event.href}
-                  className="ml-6 flex-none self-center rounded-md bg-white px-3 py-2 font-semibold text-gray-900 opacity-0 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400 focus:opacity-100 group-hover:opacity-100"
-                >
-                  Edit<span className="sr-only">, {event.name}</span>
-                </a>
-              </li>
-            ))}
-          </ol>
-        </div>
-      )} */}
       </div>
     </div>
   );
