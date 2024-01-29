e9a3a96dc9be3e3d8ae86d0e5d50395f0b120ca9
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-29 11:32:11 -0500

    Add 'matched' attribute to Mentor and Student serializers

--- app/serializers/mentor_serializer.rb
+++ app/serializers/mentor_serializer.rb
@@ -1,5 +1,5 @@
 class MentorSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email_address, :password_digest, :company_name, :job_title, :profile_image_url
+  attributes :id, :first_name, :last_name, :email_address, :password_digest, :company_name, :job_title, :profile_image_url, :matched
   has_many :meetings
   has_one :mentor_application
   has_one :student
--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,5 +1,5 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email_address, :university_name, :degree_type, :area_of_study, :expected_graduation_date, :mentor_id, :profile_image_url
+  attributes :id, :first_name, :last_name, :email_address, :university_name, :degree_type, :area_of_study, :expected_graduation_date, :mentor_id, :profile_image_url, :matched
   has_many :meetings
   has_one :student_application
   has_one :mentor
