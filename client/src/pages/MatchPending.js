31693d6e377f553f237446a6e40c7e158a64cb61
Nicholas Galante <nicholasgalante94@gmail.com>
2024-01-29 13:54:54 -0500

    Update match pending message

--- client/src/pages/MatchPending.js
+++ client/src/pages/MatchPending.js
@@ -4,7 +4,7 @@
    return (
      <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
        <div className="mx-auto max-w-2xl text-center">
-         <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">Waiting for your match...</h2>
+         <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl">We're still searching for your perfect match! </h2>
          <p className="mt-6 text-lg leading-8 text-gray-600">
            Your application has been submitted! <br/>You will be notified when you have been matched. 
          </p>
